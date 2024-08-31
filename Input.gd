5e0066d3839e3a283c186e6a9db12e462454f4fd
UnseenFaith <1832323+UnseenFaith@users.noreply.github.com>
2024-08-30 20:38:46 -0400

    replace spaces

--- Input.gd
+++ Input.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready() -> void:
 	var web_hash = JavaScriptBridge.eval("window.location.hash")
 	if web_hash:
-		var replaced_text = web_hash.replace("#", "")
+		var replaced_text = web_hash.replace("#", "").replace("%20", " ")
 		var text1 = replaced_text.split(";")[0]
 		var text2 = replaced_text.split(";")[1]
 		$TextLabel.text = $TextLabel.text.replace("{text1}", text1).replace("{text2}", text2)
