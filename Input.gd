6b2a93f873d9cdcb8fa67d1dc03775e8a2cfcb89
UnseenFaith <1832323+UnseenFaith@users.noreply.github.com>
2024-08-30 20:10:06 -0400

    test2

--- Input.gd
+++ Input.gd
@@ -1,7 +1,7 @@
 extends Control
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	JavaScriptBridge.eval("console.log(window.location.search")
+	JavaScriptBridge.eval("alert(window.location.search)")
 	if Input.is_action_just_pressed("switch"):
 		$StartingSoon.visible = !$StartingSoon.visible
 		$BeRightBack.visible = !$BeRightBack.visible
